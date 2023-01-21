e88b035e6be803e9db9c41ce9d7c86c37e165e0c
pashaeff <pashaeff@MacBook-Air-pashaeff.local>
2023-01-21 06:27:36 +0400

    User add changes

--- site_sections/user.py
+++ site_sections/user.py
@@ -28,15 +28,19 @@ def update_profile(id: int, request: Request, db: Session = Depends(database.get
                     categories = db.query(models.EduCategory).all()
                     news_category = db.query(models.NewsCategory).all()
                     profile = db.query(models.User).filter_by(id=id).first()
+                    variables = site_default_variables(request)
                     if profile:
                         page_title = profile.name_surname +" - "+lang.user_profile_info_page_title
-                        user_education_files = profile.education_files.split(',')
+                        if profile.education_files:
+                            user_education_files = profile.education_files.split(',')
+                        else:
+                            user_education_files = ""
                     else:
                         return RedirectResponse("/",status_code=HTTP_303_SEE_OTHER)
                     return templates.TemplateResponse("site/route/profile.html", {"request":request, "user":check_site_user['user'], "edu":edu, "categories":categories,
                                                                                 "current_user":check_site_user['current_user'], "profile":profile, "news_category":news_category,
                                                                                 "page_title":page_title, "site_settings":check_site_user['site_settings'],"language":lang,
-                                                                                "user_education_files":user_education_files})
+                                                                                "user_education_files":user_education_files, "flash":variables['_flash_message']})
                 else:
                     return RedirectResponse("/",status_code=HTTP_303_SEE_OTHER)
             else:
@@ -84,42 +88,40 @@ async def user_upload_file(id: int, request: Request, db: Session = Depends(data
         else:
             request.session["flash_messsage"] = []
             if check_site_user['user']:
+                user = db.query(models.User).filter_by(id=check_site_user['current_user'])
                 if id == check_site_user['user'].id:
                     if len(files) > 4:
                         request.session["flash_messsage"].append({"message": "Maksimum 4 fayl", "category": "error"})
-                        request = RedirectResponse(url=f"/user/{id}/upload",status_code=HTTP_303_SEE_OTHER)
+                        request = RedirectResponse(url=f"/user/{user.first().id}/upload",status_code=HTTP_303_SEE_OTHER)
                         return request
                     directory = check_site_user['user'].id
                     FILEPATH = f"static/user_files/{directory}/"
                     if not os.path.exists(FILEPATH):
                         os.makedirs(FILEPATH)
                     user_files = ''
-                    for file in files:
-                        if file:
+                    if files:
+                        for file in files:
                             filename = file.filename
-                            if len(filename) > 0:
-                                extension = filename.split(".")[1]
-                                if extension not in ["png","jpg","jpeg"]:
-                                    request.session["flash_messsage"].append({"message": lang.image_extension_error_message, "category": "error"})
-                                    request = RedirectResponse(url=f"/user/{id}/upload",status_code=HTTP_303_SEE_OTHER)
-                                    return request
-                                else:
-                                    token_name = secrets.token_hex(12)+"."+extension
-                                    generated_name = FILEPATH + token_name
-                                    file_content = await file.read()
-                                    with open(generated_name, "wb") as file:
-                                        file.write(file_content)
-                                    img = Image.open(generated_name)
-                                    img.save(generated_name)
-                                    user_files += token_name+","
-                                    file.close()
-                    user = db.query(models.User).filter_by(id=check_site_user['current_user'])
-                    us = user.first().education_files
-                    user.update({'education_files':user_files[:-1]},synchronize_session=False)
-                    db.commit()
-                    request.session["flash_messsage"].append({"message": 'Fayllar yuklendi', "category": "success"})
-                    request = RedirectResponse(url=f"/user/{id}/upload",status_code=HTTP_303_SEE_OTHER)
-                    return request
+                            extension = filename.split(".")[1]
+                            if extension not in ["png","jpg","jpeg"]:
+                                request.session["flash_messsage"].append({"message": lang.image_extension_error_message, "category": "error"})
+                                request = RedirectResponse(url=f"/user/{id}/upload",status_code=HTTP_303_SEE_OTHER)
+                                return request
+                            else:
+                                token_name = secrets.token_hex(12)+"."+extension
+                                generated_name = FILEPATH + token_name
+                                file_content = await file.read()
+                                with open(generated_name, "wb") as file:
+                                    file.write(file_content)
+                                img = Image.open(generated_name)
+                                img.save(generated_name)
+                                user_files += token_name+","
+                                file.close()
+                        user.update({'education_files':user_files[:-1]},synchronize_session=False)
+                        db.commit()
+                        request.session["flash_messsage"].append({"message": 'Fayllar yuklendi', "category": "success"})
+                        request = RedirectResponse(url=f"/profile/{id}",status_code=HTTP_303_SEE_OTHER)
+                        return request
 
 
 @user_panel.get("/update_profile/{id}")
@@ -204,4 +206,24 @@ async def post_update_profile(id:int, request: Request, db:Session = Depends(dat
             db.commit()
             request.session["flash_messsage"].append({"message": lang.profile_success_message, "category": "success"})
             request = RedirectResponse(url=f"/",status_code=HTTP_303_SEE_OTHER)
+            return request
+
+
+@user_panel.post("/files/{id}/delete")
+def delete_user_files(id:int, request: Request, db:Session = Depends(database.get_db)):
+    check_site_user = check_user_in_site(request)
+    if check_site_user['user']:
+        profile = db.query(models.User).filter_by(id=id)
+        user_education_files = profile.first().education_files.split(',')
+        FILEPATH = f"static/user_files/{profile.first().id}/"
+        request.session["flash_messsage"] = []
+        if id == check_site_user['user'].id or check_site_user['user'].admin_user == True or check_site_user['user'].super_user == True: 
+            profile.update({"education_files":""})
+            db.commit()
+            if len(user_education_files) > 0:
+                for image in user_education_files:
+                    delete_image = pathlib.Path(FILEPATH+image)
+                    delete_image.unlink()
+            request.session["flash_messsage"].append({"message": "Silindi", "category": "success"})
+            request = RedirectResponse(url=f"/profile/{profile.first().id}",status_code=HTTP_303_SEE_OTHER)
             return request
